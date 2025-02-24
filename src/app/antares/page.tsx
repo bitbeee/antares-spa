3dca897b7812539e277781d9c7876ecb4a528262
Bruno Santos <bruno.cristiano.santos@gmail.com>
2025-02-24 14:05:14 -0300

    add caroseul

--- src/app/antares/page.tsx
+++ src/app/antares/page.tsx
@@ -5,6 +5,7 @@ import Header from "@/components/Header";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 import About from "./About";
+import Carousel from "./Carousel";
 import Location from "./Location";
 import Services from "./Service";
 
@@ -12,6 +13,7 @@ export default function Antares() {
   return (
     <div className="h-full flex flex-col gap-x-7 ">
       <Header isAntaresPage={true} />
+      <Carousel />
       <About />
       <Services />
       <Location />
